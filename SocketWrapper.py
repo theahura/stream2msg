75a1f7a54689d9bd62950d88676e066e24d94912
Amol Kapoor <theahura@gmail.com>
2018-04-03 02:18:21 -0400

    Added some better error handling

--- SocketWrapper.py
+++ SocketWrapper.py
@@ -8,12 +8,20 @@ import socket
 
 
 class SocketWrapper(object):
+    """Wrapper for low level socket that handles sending/receiving messages."""
 
-    def __init__(self, is_listener, socket_info, socket=None):
+    def __init__(self, is_listener, socket_info, user_socket=None):
+        """Initializes a socket wrapper.
+
+        @param is_listener: bool, socket is a listener if true.
+        @param socket_info: (ip, port)
+        @param socket: a user-defined socket for custom socket behaviors.
+        """
+        self.closed = False
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        if socket:
-            self.socket = socket
+        if user_socket:
+            self.socket = user_socket
         elif not is_listener:
             self.socket = s
             self.socket.connect(socket_info)
@@ -24,20 +32,22 @@ class SocketWrapper(object):
             s.close()
 
     def send_data(self, data, metadata=[]):
+        """Sends string data through socket.
+
+        Tags data with length. Metadata is tagged in front as strings. Typing
+        needs to be tracked by the user on the receiving end."""
+
+        if self.closed:
+            raise ConnectionRefusedError('Socket closed')
 
         if type(data) != str:
             raise ValueError('Send data expects string data')
-
         data = '---'.join([str(md) for md in metadata]) + '---' + data
-
-        if len(str(len(data))) > 10:
+        if len(str(len(data))) > 10 - 3:
             raise ValueError('Data being sent is too large')
-
         data = str(len(data)) + '___' + data
-
-        if len(data) < 10:
+        if len(data) <= 10:
             raise ValueError('Data being sent is too small')
-
         try:
             self.socket.send(data)
         except Exception as e:
@@ -46,38 +56,33 @@ class SocketWrapper(object):
             raise
 
     def close_socket(self):
-        try:
-            self.socket.send('10_killthread')
-        except socket.error, e:
-            print e
-            pass
+        """Closes the socket and attempts to notify other socket of close."""
+        self.closed = True
         self.socket.close()
 
     def get_message(self):
+        """Gets a message.
+
+        Grabs the first 10 chars to get length of the message, then gets the
+        rest. Returns an array with metadata in front and the data in the last
+        index."""
+        if self.closed:
+            raise ConnectionRefusedError('Socket closed')
         data_str = self.socket.recv(10)
-
         if not data_str:
+            self.close_socket()
             return None
-
         message_size, data_str = data_str.split('___', 1)
         message_size = int(message_size)
-
         while len(data_str) < message_size:
             new_data = self.socket.recv(
                 min(4096, message_size - len(data_str)))
-
             if not new_data:
+                self.close_socket()
                 return None
-
             data_str += new_data
-
-        if data_str == 'killthread':
-            self.socket.close()
-            return None
-
         if '---' in data_str:
             data = data_str.split('---')
         else:
             data = [data_str]
-
         return data
